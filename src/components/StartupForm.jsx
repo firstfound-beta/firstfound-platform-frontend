2604354a7ba92756ca480e57fd433cd701530e61
Jeet267 <ajtkr200@gmail.com>
2025-08-04 03:28:40 +0530

    change

--- src/components/StartupForm.jsx
+++ src/components/StartupForm.jsx
@@ -11,9 +11,7 @@ function StartupForm() {
     website: "",
     pitchDeckUrl: "",
   });
-
   const [message, setMessage] = useState("");
-
   const handleChange = (e) => {
     setFormData((prev) => ({
       ...prev,
